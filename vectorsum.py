ade24b6d13adbc78f8181e166e96c423f27768a0
bolan2014 <1139217488@qq.com>
2015-11-05 21:15:09 +0800

    first commit

---
+++ vectorsum.py
@@ -0,0 +1,34 @@
+import sys
+from datetime import datetime
+import numpy as np
+
+def numpysum(n):
+    a = np.arange(n) ** 2
+    b = np.arange(n) ** 3
+    c = a + b
+    return c
+
+def pythonsum(n):
+    a = range(n)
+    b = range(n)
+    c = []
+
+    for i in range(len(a)):
+        a[i] = i ** 2
+        b[i] = i ** 3
+        c.append(a[i] + b[i])
+    return c
+
+size = int(sys.argv[1])
+
+start = datetime.now()
+c = pythonsum(size)
+delta = datetime.now() - start
+print "The last 2 elements of the sum", c[-2:]
+print "PythonSum elapsed time in microseconds", delta.microseconds
+
+start = datetime.now()
+c = numpysum(size)
+delta = datetime.now() - start
+print "The last 2 elements of the sum", c[-2:]
+print "NumPySum elapsed time in microseconds", delta.microseconds
