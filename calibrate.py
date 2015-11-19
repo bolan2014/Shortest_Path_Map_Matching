f35457abe47a3974ee7f89627ffcb5ab393c9799
bolan2014 <1139217488@qq.com>
2015-11-19 21:52:57 +0800

    update

--- calibrate.py
+++ calibrate.py
@@ -1,54 +1,51 @@
-import tarfile
-import os
-import time
-import shutil
-
-from pyproj import Proj, transform
 import numpy as np
+from math import sqrt,sin,cos
 
-def decompressiontar(sourcepath, despath):
-	tar = tarfile.open(sourcepath)
-	names = tar.getnames()
-	for name in names:
-		tar.extract(name, path = despath)
-	tar.close()
+#GPS point transforming process, only works in PRC.
+pi=3.14159265358979324
+a=6378245.0
+ee=0.00669342162296594323
 
-def buildlist(sourcepath_name):
-    with open(sourcepath_name, "r") as f:
-        line = f.readline()
-        columns = []
-        while True:
-            line = f.readline()
-            if not line:
-                f.close()
-                return columns
-            columns.append(line.split(','))
+def transforlat(x,y):
+    ret=-100.0+2.0*x+3.0*y+0.2*y*y+0.1*x*y+0.2*sqrt(abs(x))
+    ret+=(20.0*sin(6.0*x*pi)+20.0*sin(2.0*x*pi))*2.0/3.0
+    ret+=(20.0*sin(y*pi)+40.0*sin(y/3.0*pi))*2.0/3.0
+    ret+=(160.0*sin(y/12.0*pi)+320.0*sin(y/30.0*pi))*2.0/3.0
+    return ret
 
-# transform from epsg 4326 to 3785
-def trans(x, y):
-	epsg4326 = Proj(proj='utm', zone=50, ellps='WGS84')
-	epsg3785 = Proj(init = 'epsg:3785')
-	x1, y1 = epsg4326(x, y)
-	x2, y2 = transform(epsg4326, epsg3785, x1, y1)
-	return x2, y2
+def transforlon(x,y):
+    ret=300.0+x+2.0*y++0.1*x*x+0.1*x*y+0.1*sqrt(abs(x))
+    ret+=(20.0*sin(6.0*x*pi)+20.0*sin(2.0*x*pi))*2.0/3.0
+    ret+=(20.0*sin(x*pi)+40.0*sin(x/3.0*pi))*2.0/3.0
+    ret+=(150.0*sin(x/12.0*pi)+300.0*sin(x/30.0*pi))*2.0/3.0
+    return ret   
 
+def transform(lon,lat):
+    dlat=transforlat(lon-105.0,lat-35.0)
+    dlon=transforlon(lon-105.0,lat-35.0)
+    radlat=lat/180.0*pi
+    magic=sin(radlat)
+    magic=1-ee*magic*magic
+    sqrtmagic=sqrt(magic)
+    dlat=(dlat*180.0)/((a*(1-ee))/(magic*sqrtmagic)*pi)
+    dlon=(dlon*180.0)/(a/sqrtmagic*cos(radlat)*pi)
+    gcjlat=lat+dlat
+    gcjlon=lon+dlon
+    return gcjlon,gcjlat
+
+#main process on txt
 def main():
-
-    x, y = np.loadtxt('ins.txt', delimiter=',', usecols=(4,5), unpack=True)
-
-    # epsg4236to3785(x, y)
-
-    for i in range(len(x)):
-        x[i], y[i] = trans(x[i], y[i])
-
-    rst = np.column_stack((x, y))
-    # print rst
-
+    xIn, yIn = np.loadtxt('taxi.txt', delimiter=',', usecols=(4,5), unpack=True)
+    print "The point number is:",len(xIn)
+    length=len(xIn)
+    for i in range(length):
+        xIn[i],yIn[i]=transform(xIn[i],yIn[i])
+        #if i%(length/10)==0:
+        #    print i*10/length
+    rst = np.column_stack((xIn, yIn))
     np.savetxt('rst.csv', rst, delimiter=",", fmt="%s")
     print ("Transformation completed.")
 
-if __name__ == '__main__':
-	#start = time.time()
-	main()
-	#end = time.time()
-	#print(end - start)
+if __name__ == "__main__":
+    main()
+
