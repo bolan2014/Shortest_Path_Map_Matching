31083c336e50207424c9de4eb7450b3ad2c823fd
bolan2014 <1139217488@qq.com>
2015-12-28 14:24:38 +0800

    remove unnecessary code

--- mapMatch/read.py
+++ mapMatch/read.py
@@ -5,54 +5,16 @@ reading info of link, node, track
 import SHlink
 import TrackPoint
 
-'''def ReadingDRMlink(linklist, linkID, fname):
-    fl = open(fname, 'r')
-    
-    while True:
-        line = fl.readline()
-        if not line:
-            break
-        link = SHlink.SHlink(line)
-        linklist[link.linkid] = link
-        linkID.append(link.linkid)
-
-    fl.close()
-
-def ReadingDRMnode(nodelist, fname):
-    fn = open(fname, 'r')
-
-    while True:
-        line = fn.readline()
-        if not line:
-            break
-        node = SHlink.SHlink(line)
-        nodelist[node.node1] = node
-
-    fn.close()
-    '''
 
 def ReadingDRMinfo(fname, linklist, linkID, nodelist):
     fr = open(fname, 'r')
+
     for line in fr:
         link = SHlink.SHlink(line)
         linklist[link.linkid] = link
         linkID.append(link.linkid)
 
-        nodelist[node.node1] = link
+        nodelist[link.node1] = link
     fr.close()
-
-'''def ReadingTrackInfo(tracklist, tracktime, fname):
-    ft = open(fname, 'r')
-
-    while True:
-        line = ft.readline()
-        if not line:
-            break
-        record = TrackPoint.TrackPoint(line)
-        tracklist[record.datetime] = record
-        tracktime.append(record.datetime)
-
-    ft.close()
-    '''
 
 ''' global: nodelist linklist linkID tracktime tracklist '''
